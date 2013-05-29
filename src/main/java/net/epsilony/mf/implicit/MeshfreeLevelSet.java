0b9e703474e29c97c4c4064a9acf377b1997ce3c
Man YUAN <epsilonyuan@gmail.com>
2013-05-29 13:42:42 +0800

    extract MeshfreeLevelSet from MLSLevelSetDemo

---
+++ src/main/java/net/epsilony/mf/implicit/MeshfreeLevelSet.java
@@ -0,0 +1,74 @@
+/*
+ * (c) Copyright by Man YUAN
+ */
+package net.epsilony.mf.implicit;
+
+import net.epsilony.mf.model.Model2D;
+import net.epsilony.mf.model.influence.InfluenceRadiusCalculator;
+import net.epsilony.mf.process.PostProcessor;
+import net.epsilony.mf.process.WeakformProcessor;
+import net.epsilony.mf.process.WeakformQuadratureTask;
+import net.epsilony.tb.GenericFunction;
+import net.epsilony.tb.shape_func.MLS;
+import net.epsilony.tb.shape_func.RadialFunctionCore;
+import net.epsilony.tb.shape_func.ShapeFunction;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class MeshfreeLevelSet {
+
+    LevelSetApproximationAssemblier assemblier = new LevelSetApproximationAssemblier();
+    ShapeFunction shapeFunction = new MLS();
+    WeakformProcessor weakformProcessor = new WeakformProcessor();
+
+    public void setWeightFunction(RadialFunctionCore weightFunction) {
+        assemblier.setWeightFunction(weightFunction);
+    }
+
+    public void setWeakformQuadratureTask(WeakformQuadratureTask weakformQuadratureTask) {
+        weakformProcessor.setWeakformQuadratureTask(weakformQuadratureTask);
+    }
+
+    public void setModel(Model2D model) {
+        weakformProcessor.setModel(model);
+    }
+
+    public void setInfluenceRadiusCalculator(InfluenceRadiusCalculator influenceRadiusCalculator) {
+        weakformProcessor.setInfluenceRadiusCalculator(influenceRadiusCalculator);
+    }
+
+    public void setShapeFunction(ShapeFunction shapeFunction) {
+        this.shapeFunction = shapeFunction;
+    }
+
+    public void prepare() {
+
+
+        weakformProcessor.setAssemblier(assemblier);
+
+        weakformProcessor.setShapeFunction(shapeFunction);
+        weakformProcessor.prepare();
+
+        weakformProcessor.process();
+        weakformProcessor.solve();
+    }
+
+    public GenericFunction<double[], double[]> getLevelSetFunction() {
+        final PostProcessor postProcessor = weakformProcessor.postProcessor();
+        return new GenericFunction<double[], double[]>() {
+            @Override
+            public double[] value(double[] input, double[] output) {
+                postProcessor.setDiffOrder(0);
+                double[] result = postProcessor.value(input, null);
+                if (null == output) {
+                    return result;
+                } else {
+                    System.arraycopy(result, 0, output, 0, result.length);
+                    return output;
+                }
+            }
+        };
+    }
+}
--- src/main/java/net/epsilony/mf/implicit/demo/MLSLevelSetDemo.java
+++ src/main/java/net/epsilony/mf/implicit/demo/MLSLevelSetDemo.java
@@ -8,19 +8,14 @@ import java.awt.Dimension;
 import java.awt.geom.Rectangle2D;
 import java.util.LinkedList;
 import java.util.List;
-import net.epsilony.mf.implicit.LevelSetApproximationAssemblier;
+import net.epsilony.mf.implicit.MeshfreeLevelSet;
 import net.epsilony.mf.implicit.RectangleWithHoles;
 import net.epsilony.tb.implicit.TriangleContourBuilder;
 import net.epsilony.tb.implicit.TriangleContourCell;
 import net.epsilony.tb.implicit.TriangleContourCellFactory;
 import net.epsilony.mf.model.influence.ConstantInfluenceRadiusCalculator;
 import net.epsilony.mf.model.influence.InfluenceRadiusCalculator;
-import net.epsilony.mf.process.PostProcessor;
-import net.epsilony.mf.process.WeakformProcessor;
-import net.epsilony.tb.shape_func.MLS;
-import net.epsilony.tb.GenericFunction;
 import net.epsilony.tb.MiscellaneousUtils;
-import net.epsilony.tb.NeedPreparation;
 import net.epsilony.tb.common_func.NormalFunction;
 import net.epsilony.tb.ui.CommonFrame;
 
@@ -28,7 +23,7 @@ import net.epsilony.tb.ui.CommonFrame;
  *
  * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
  */
-public class MLSLevelSetDemo implements NeedPreparation {
+public class MLSLevelSetDemo {
 
     public static double DEFAULT_SIGMA = 20;
     public static Rectangle2D DEFAULT_RECTANGLE = new Rectangle2D.Double(10, 10, 100, 60);
@@ -41,60 +36,43 @@ public class MLSLevelSetDemo implements NeedPreparation {
     public static double DEFAULT_SEGMENT_SIZE = 2;
     NormalFunction assemblyWeightFunction = new NormalFunction();
     double sigma = DEFAULT_SIGMA;
-    LevelSetApproximationAssemblier assemblier = new LevelSetApproximationAssemblier();
-    MLS mls = new MLS();
-    WeakformProcessor weakformProcessor = new WeakformProcessor();
+
     RectangleWithHoles rectangleWithHoles = new RectangleWithHoles(
             DEFAULT_RECTANGLE, DEFAULT_HOLE_RADIUS, DEFAULT_HOLE_DISTANCE);
     double triangleSize = DEFAULT_TRIANGLE_SIZE;
     double segmentSize = DEFAULT_SEGMENT_SIZE;
     int quadraturePower = DEFAULT_QUADRATURE_POWER;
+    MeshfreeLevelSet levelSetFun = new MeshfreeLevelSet();
 
-    @Override
-    public void prepare() {
+    public void init() {
         assemblyWeightFunction.setSigma(sigma);
-        assemblier.setWeightFunction(assemblyWeightFunction);
 
         rectangleWithHoles.setQuadraturePower(quadraturePower);
         rectangleWithHoles.setSegmentSize(segmentSize);
         rectangleWithHoles.setTriangleSize(triangleSize);
         rectangleWithHoles.prepare();
 
-        weakformProcessor.setInfluenceRadiusCalculator(DEFAULT_INFLUENCE_RADIUS_CALCULATOR);
-        weakformProcessor.setAssemblier(assemblier);
-        weakformProcessor.setModel(rectangleWithHoles.getModel());
-        weakformProcessor.setWeakformQuadratureTask(rectangleWithHoles.getWeakformQuadratureTask());
+        levelSetFun.setInfluenceRadiusCalculator(DEFAULT_INFLUENCE_RADIUS_CALCULATOR);
+        levelSetFun.setModel(rectangleWithHoles.getModel());
+        levelSetFun.setWeakformQuadratureTask(rectangleWithHoles.getWeakformQuadratureTask());
+        levelSetFun.setInfluenceRadiusCalculator(DEFAULT_INFLUENCE_RADIUS_CALCULATOR);
+        levelSetFun.setWeightFunction(assemblyWeightFunction);
 
-        weakformProcessor.setShapeFunction(mls);
-        //weakformProcessor.setEnableMultiThread(false);
-        weakformProcessor.prepare();
+        levelSetFun.prepare();
     }
 
     public static void main(String[] args) {
         MLSLevelSetDemo demo = new MLSLevelSetDemo();
-        demo.prepare();
-        demo.weakformProcessor.process();
-        demo.weakformProcessor.solve();
-        final PostProcessor postProcessor = demo.weakformProcessor.postProcessor();
-        postProcessor.setDiffOrder(0);
+        demo.init();
+
+
         TriangleContourCellFactory cellFactory = new TriangleContourCellFactory();
         TriangleContourCell[][] coverRectangle = cellFactory.coverRectangle(demo.rectangleWithHoles.getRectangle(), 1);
         TriangleContourBuilder contourBuilder = new TriangleContourBuilder();
         List<TriangleContourCell> cells = new LinkedList<>();
         MiscellaneousUtils.addToList(coverRectangle, cells);
         contourBuilder.setCells(cells);
-        contourBuilder.setLevelSetFunction(new GenericFunction<double[], double[]>() {
-            @Override
-            public double[] value(double[] input, double[] output) {
-                double[] result = postProcessor.value(input, null);
-                if (null != output) {
-                    output[0] = result[0];
-                } else {
-                    output = result;
-                }
-                return output;
-            }
-        });
+        contourBuilder.setLevelSetFunction(demo.levelSetFun.getLevelSetFunction());
 
         contourBuilder.genContour();
         CommonFrame frame = new CommonFrame();
