0b184c688990e1cd3d7f06ea295a0181859dde21
Man YUAN <epsilonyuan@gmail.com>
2013-06-25 15:57:37 +0800

    add MFNode

---
+++ src/main/java/net/epsilony/mf/model/MFNode.java
@@ -0,0 +1,57 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.model;
+
+import net.epsilony.tb.solid.Node;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class MFNode extends Node {
+
+    double influenceRadius;
+    int assemblyIndex = -1;
+    int lagrangeAssemblyIndex = -1;
+    double[] value;
+    double[] lagrangleValue;
+
+    public double getInfluenceRadius() {
+        return influenceRadius;
+    }
+
+    public void setInfluenceRadius(double influenceRadius) {
+        this.influenceRadius = influenceRadius;
+    }
+
+    public int getAssemblyIndex() {
+        return assemblyIndex;
+    }
+
+    public void setAssemblyIndex(int assemblyIndex) {
+        this.assemblyIndex = assemblyIndex;
+    }
+
+    public int getLagrangeAssemblyIndex() {
+        return lagrangeAssemblyIndex;
+    }
+
+    public void setLagrangeAssemblyIndex(int lagrangeAssemblyIndex) {
+        this.lagrangeAssemblyIndex = lagrangeAssemblyIndex;
+    }
+
+    public double[] getValue() {
+        return value;
+    }
+
+    public void setValue(double[] value) {
+        this.value = value;
+    }
+
+    public double[] getLagrangleValue() {
+        return lagrangleValue;
+    }
+
+    public void setLagrangleValue(double[] lagrangleValue) {
+        this.lagrangleValue = lagrangleValue;
+    }
+}
