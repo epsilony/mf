0d8a82e3d02ced362ea2c42c99e38c7061d44248
Man YUAN <epsilonyuan@gmail.com>
2013-07-14 16:33:51 +0800

    add interface ProcessResult and a simple implementation

---
+++ src/main/java/net/epsilony/mf/process/ProcessResult.java
@@ -0,0 +1,24 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.process;
+
+import java.util.List;
+import net.epsilony.mf.model.MFNode;
+import no.uib.cipr.matrix.DenseVector;
+import no.uib.cipr.matrix.Matrix;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public interface ProcessResult {
+
+    Matrix getMainMatrix();
+
+    boolean isUpperSymmetric();
+
+    DenseVector getGeneralForce();
+
+    List<MFNode> getNodes();
+
+    int getNodeValueDimension();
+}
---
+++ src/main/java/net/epsilony/mf/process/SimpProcessResult.java
@@ -0,0 +1,65 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.process;
+
+import java.util.List;
+import net.epsilony.mf.model.MFNode;
+import no.uib.cipr.matrix.DenseVector;
+import no.uib.cipr.matrix.Matrix;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class SimpProcessResult implements ProcessResult {
+
+    Matrix mainMatrix;
+    boolean upperSymmetric;
+    DenseVector generalForce;
+    int nodeValueDimension;
+    List<MFNode> nodes;
+
+    @Override
+    public Matrix getMainMatrix() {
+        return mainMatrix;
+    }
+
+    public void setMainMatrix(Matrix mainMatrix) {
+        this.mainMatrix = mainMatrix;
+    }
+
+    @Override
+    public boolean isUpperSymmetric() {
+        return upperSymmetric;
+    }
+
+    public void setUpperSymmetric(boolean upperSymmetric) {
+        this.upperSymmetric = upperSymmetric;
+    }
+
+    @Override
+    public DenseVector getGeneralForce() {
+        return generalForce;
+    }
+
+    public void setGeneralForce(DenseVector generalForce) {
+        this.generalForce = generalForce;
+    }
+
+    @Override
+    public int getNodeValueDimension() {
+        return nodeValueDimension;
+    }
+
+    public void setNodeValueDimension(int nodeValueDimension) {
+        this.nodeValueDimension = nodeValueDimension;
+    }
+
+    @Override
+    public List<MFNode> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<MFNode> nodes) {
+        this.nodes = nodes;
+    }
+}
