5f3c99ade8cd8e2cd42bc1f845b575eb47313149
Man YUAN <epsilonyuan@gmail.com>
2013-08-20 21:47:37 +0800

    redesign mls function cache to support 1d-3d, only be 2d tests there

--- src/main/java/net/epsilony/mf/shape_func/MLS.java
+++ src/main/java/net/epsilony/mf/shape_func/MLS.java
@@ -1,19 +1,20 @@
 /* (c) Copyright by Man YUAN */
 package net.epsilony.mf.shape_func;
 
-import net.epsilony.tb.common_func.RadialFunction2D;
-import net.epsilony.tb.common_func.MonomialBasis2D;
-import net.epsilony.tb.common_func.BasisFunction;
 import gnu.trove.list.array.TDoubleArrayList;
 import java.util.List;
 import net.epsilony.mf.geomodel.MFNode;
-import net.epsilony.tb.analysis.Math2D;
+
 import net.epsilony.tb.MiscellaneousUtils;
+import net.epsilony.tb.analysis.Dimensional;
 import net.epsilony.tb.analysis.WithDiffOrderUtil;
+import net.epsilony.tb.common_func.BasesFunction;
+import net.epsilony.tb.common_func.MonomialBases;
+
+import net.epsilony.tb.common_func.RadialBasis;
+
 import net.epsilony.tb.synchron.SynchronizedClonable;
-import org.ejml.alg.dense.decomposition.lu.LUDecompositionAlt;
-import org.ejml.alg.dense.linsol.lu.LinearSolverLu;
-import org.ejml.alg.dense.mult.MatrixVectorMult;
+import org.apache.commons.math3.util.FastMath;
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
 
@@ -21,22 +22,34 @@ import org.ejml.ops.CommonOps;
  *
  * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
  */
-public class MLS implements MFShapeFunction, SynchronizedClonable<MFShapeFunction> {
+public class MLS implements Dimensional, MFShapeFunction, SynchronizedClonable<MFShapeFunction> {
 
-    public static final int DIMENSION = 2;
-    RadialFunction2D weightFunc;
-    BasisFunction basisFunc;
-    DenseMatrix64F[] matAs;
-    public final int DEFAULT_CAPACITY_OF_INPUT_COORDS = 50;
-    TDoubleArrayList[][] matBs;
-    TDoubleArrayList[] distsCache;
-    TDoubleArrayList[] weightsCache;
-    TDoubleArrayList[] basisCache;
-    TDoubleArrayList commonCache;
-    LinearSolverLu solver = new LinearSolverLu(new LUDecompositionAlt());
-    DenseMatrix64F gamma, gamma_d, tVec1, tVec2;
-    TDoubleArrayList[] shapeFunctionValueLists;
-    static private final double[] ZERO = new double[]{0, 0};
+    RadialBasis weightFunc = new RadialBasis();
+    BasesFunction<? extends BasesFunction> basesFunc = new MonomialBases();
+    MLSCache cache = new MLSCache();
+    double[] zeros = new double[2];
+    private TDoubleArrayList[] dists;
+    private List<MFNode> nodes;
+    private double[] pos;
+
+    @Override
+    public void setDimension(int dim) {
+        _setDimension(dim);
+    }
+
+    private void _setDimension(int dim) {
+        if (dim < 1 || dim > 3) {
+            throw new IllegalArgumentException("only supports dim 1-3, not " + dim);
+        }
+        weightFunc.setDimension(dim);
+        basesFunc.setDimension(dim);
+        zeros = new double[dim];
+    }
+
+    @Override
+    public int getDimension() {
+        return weightFunc.getDimension();
+    }
 
     @Override
     public int getDiffOrder() {
@@ -45,214 +58,118 @@ public class MLS implements MFShapeFunction, SynchronizedClonable<MFShapeFunctio
 
     @Override
     public void setDiffOrder(int diffOrder) {
+        _setDiffOrder(diffOrder);
+    }
+
+    private void _setDiffOrder(int diffOrder) {
         if (diffOrder < 0 || diffOrder > 1) {
             throw new IllegalArgumentException("only support diffOrder that is 0 or 1, not " + diffOrder);
         }
         weightFunc.setDiffOrder(diffOrder);
-        buildCaches();
-    }
-
-    private void buildCaches() {
-        int diffSize = WithDiffOrderUtil.outputLength2D(getDiffOrder());
-        matAs = new DenseMatrix64F[diffSize];
-        distsCache = new TDoubleArrayList[diffSize];
-        basisCache = new TDoubleArrayList[diffSize];
-
-        int basisLen = basisFunc.basisLength();
-        weightsCache = new TDoubleArrayList[diffSize];
-        gamma = new DenseMatrix64F(basisLen, 1);
-        gamma_d = new DenseMatrix64F(basisLen, 1);
-        tVec1 = new DenseMatrix64F(basisLen, 1);
-        tVec2 = new DenseMatrix64F(basisLen, 1);
-        commonCache = new TDoubleArrayList(DEFAULT_CAPACITY_OF_INPUT_COORDS);
-        for (int i = 0; i < diffSize; i++) {
-            matAs[i] = new DenseMatrix64F(basisLen, basisLen);
-            distsCache[i] = new TDoubleArrayList(DEFAULT_CAPACITY_OF_INPUT_COORDS);
-            weightsCache[i] = new TDoubleArrayList(DEFAULT_CAPACITY_OF_INPUT_COORDS);
-            basisCache[i] = new TDoubleArrayList(basisLen);
-        }
-        matBs = new TDoubleArrayList[diffSize][basisLen];
-        for (int i = 0; i < matBs.length; i++) {
-            TDoubleArrayList[] matB = matBs[i];
-            for (int j = 0; j < matB.length; j++) {
-                matB[j] = new TDoubleArrayList(DEFAULT_CAPACITY_OF_INPUT_COORDS);
-            }
-        }
-        shapeFunctionValueLists = WithDiffOrderUtil.initOutput(
-                null,
-                DEFAULT_CAPACITY_OF_INPUT_COORDS,
-                DIMENSION,
-                getDiffOrder());
-    }
-
-    private void resetCaches(int capacity) {
-        for (int i = 0; i < distsCache.length; i++) {
-            distsCache[i].resetQuick();
-            distsCache[i].ensureCapacity(capacity);
-            matAs[i].zero();
-            TDoubleArrayList[] bMat = matBs[i];
-            for (int j = 0; j < bMat.length; j++) {
-                bMat[j].resetQuick();
-                bMat[j].ensureCapacity(capacity);
-            }
-        }
-        commonCache.resetQuick();
-        commonCache.ensureCapacity(capacity);
-        shapeFunctionValueLists = WithDiffOrderUtil.initOutput(
-                shapeFunctionValueLists,
-                capacity,
-                DIMENSION,
-                getDiffOrder());
-    }
-
-    public MLS(RadialFunction2D weightFunc, BasisFunction basisFunc) {
-        this.weightFunc = weightFunc;
-        this.basisFunc = basisFunc;
-        weightFunc.setDiffOrder(0);
-        buildCaches();
-    }
-
-    public MLS(RadialFunction2D weightFunc) {
-        this.weightFunc = weightFunc;
-        this.basisFunc = new MonomialBasis2D(2);
-        weightFunc.setDiffOrder(0);
-        buildCaches();
     }
 
     public MLS() {
-        this.weightFunc = new RadialFunction2D();
-        this.basisFunc = new MonomialBasis2D(2);
-        weightFunc.setDiffOrder(0);
-        buildCaches();
+        _setDiffOrder(0);
+        _setDimension(2);
     }
 
     @Override
     public TDoubleArrayList[] values(
-            double[] xy,
+            double[] pos,
             List<MFNode> nodes,
             TDoubleArrayList[] dists) {
-        resetCaches(nodes.size());
-        if (null == dists) {
-            dists = ordinaryDistances(xy, nodes);
+        int nodesSize = nodes.size();
+        int diffOrder = getDiffOrder();
+        int dimension = getDimension();
+        cache.setup(diffOrder, dimension, basesFunc.basesSize(), nodesSize);
+        this.dists = dists;
+        this.pos = pos;
+        this.nodes = nodes;
+        calcMatAB();
+
+        basesFunc.setDiffOrder(diffOrder);
+
+        int numDiffs = WithDiffOrderUtil.outputLength(dimension, diffOrder);
+        DenseMatrix64F[] resultsWraps = new DenseMatrix64F[numDiffs];
+        for (int i = 0; i < resultsWraps.length; i++) {
+            resultsWraps[i] = new DenseMatrix64F(nodesSize, 1);
         }
 
-        calcMatAB(xy, nodes, dists);
 
-        int diffOrder = getDiffOrder();
-        basisFunc.setDiffOrder(diffOrder);
-        basisFunc.values(ZERO, basisCache);
-        solver.setA(matAs[0]);
-        copyTo(basisCache[0], tVec1);
-        solver.solve(tVec1, gamma);
-        dot(matBs[0], gamma, shapeFunctionValueLists[0]);
+        DenseMatrix64F gamma = cache.getGammaCache(0);
+        double[][] basesByDiff = cache.getBasesCache();
+        basesFunc.values(zeros, basesByDiff);
+        DenseMatrix64F[] basesByDiffWrap = cache.getBasesCacheWraper();
+        DenseMatrix64F matA = cache.getMatACache(0);
+
+
+        solve(matA, basesByDiffWrap[0], gamma);
+        DenseMatrix64F matB = cache.getMatBCache(0);
+        CommonOps.multTransA(matB, gamma, resultsWraps[0]);
 
         if (diffOrder > 0) {
-            for (int i = 1; i < matAs.length; i++) {
-                MatrixVectorMult.mult(matAs[i], gamma, tVec1);
-                copyTo(basisCache[i], tVec2);
-                CommonOps.sub(tVec2, tVec1, tVec1);
-                solver.solve(tVec1, gamma_d);
-                dot(matBs[i], gamma, shapeFunctionValueLists[i]);
-                commonCache.resetQuick();
-                dot(matBs[0], gamma_d, commonCache);
-                addTo(commonCache, shapeFunctionValueLists[i]);
+            DenseMatrix64F tv = cache.getGammaCache(numDiffs);
+            
+            DenseMatrix64F tv2 = cache.getMatBCache(numDiffs);  //magic to get a matrix with more space than nodesSizes
+            tv2.numRows = nodesSize;
+            tv2.numCols = 1;
+            
+            DenseMatrix64F gamma_d = cache.getGammaCache(1);
+            for (int i = 1; i < numDiffs; i++) {
+                CommonOps.mult(cache.getMatACache(i), gamma, tv);
+                CommonOps.add(-1, tv, basesByDiffWrap[i], tv);
+                solve(matA, tv, gamma_d);
+                CommonOps.multTransA(matB, gamma_d, tv2);
+                DenseMatrix64F matB_d = cache.getMatBCache(i);
+                CommonOps.multTransA(matB_d, gamma, resultsWraps[i]);
+                CommonOps.addEquals(resultsWraps[i], tv2);
             }
         }
-        
-        if(shapeFunctionValueLists[0].contains(Double.NaN)){
-            throw new IllegalStateException();   //TODO: only a weak check for singular matrix here
+
+        for (int i = 0; i < resultsWraps[0].getNumElements(); i++) {
+            double v = resultsWraps[0].data[i];
+            if (Double.isInfinite(v) || Double.isNaN(v)) {
+                throw new IllegalStateException();
+            }
         }
+
+        TDoubleArrayList[] shapeFunctionValueLists = new TDoubleArrayList[numDiffs];
+        for (int i = 0; i < numDiffs; i++) {
+            shapeFunctionValueLists[i] = new TDoubleArrayList(resultsWraps[i].data);
+        }
+
         return shapeFunctionValueLists;
     }
 
-    private void calcMatAB(
-            double[] xy,
-            List<MFNode> nodes,
-            TDoubleArrayList[] dists) {
-        basisFunc.setDiffOrder(0);
-        double[] tds = new double[2];
-        int coordIndex = 0;
-        double[] weights = new double[matAs.length];
-        double[] ds = new double[dists.length];
-        for (MFNode node : nodes) {
-            basisFunc.values(Math2D.subs(node.getCoord(), xy, tds), basisCache);
-            TDoubleArrayList basis = basisCache[0];
-            for (int j = 0; j < dists.length; j++) {
-                ds[j] = dists[j].getQuick(coordIndex);
+    private void calcMatAB() {
+        int nodeIndex = 0;
+        initMatAB();
+        double[] weights = new double[1 + getDimension()];
+        double[] dist = new double[1 + getDimension()];
+        double[] tCoord = new double[getDimension()];
+        basesFunc.setDiffOrder(0);
+        double[][] bases = cache.getBasesCache();
+        for (MFNode nd : nodes) {
+            getDist(nd, nodeIndex, dist);
+            double infRad = nd.getInfluenceRadius();
+            weightFunc.values(dist, infRad, weights);
+            for (int i = 0; i < tCoord.length; i++) {
+                tCoord[i] = nd.getCoord()[i] - pos[i];
             }
-            weightFunc.values(ds, node.getInfluenceRadius(), weights);
-            for (int differential = 0; differential < matAs.length; differential++) {
-                DenseMatrix64F A_d = matAs[differential];
-                TDoubleArrayList[] B_d = matBs[differential];
-                double weight = weights[differential];
-                vectorTransMultVectorAddtoA(weight, basis, A_d);
-                appendToB(weight, basis, B_d);
-            }
-            coordIndex++;
+            basesFunc.values(tCoord, bases);
+            pushToMatA(weights, cache.getBasesCacheWraper()[0]);
+            pushToMatB(weights, cache.getBasesCacheWraper()[0], nodeIndex);
+            nodeIndex++;
         }
-    }
-
-    static private void addTo(TDoubleArrayList from, TDoubleArrayList to) {
-        for (int i = 0; i < from.size(); i++) {
-            to.setQuick(i, to.getQuick(i) + from.getQuick(i));
-        }
-    }
-
-    static private void dot(TDoubleArrayList[] mat, DenseMatrix64F vec, TDoubleArrayList result) {
-        for (int i = 0; i < mat[0].size(); i++) {
-            double sum = 0;
-            for (int j = 0; j < mat.length; j++) {
-                sum += vec.get(j) * mat[j].getQuick(i);
-            }
-            result.add(sum);
-        }
-    }
-
-    static private void copyTo(TDoubleArrayList from, DenseMatrix64F to) {
-        for (int i = 0; i < from.size(); i++) {
-            to.set(i, from.getQuick(i));
-        }
-    }
-
-    static private void vectorTransMultVectorAddtoA(double scale, TDoubleArrayList vector, DenseMatrix64F matA) {
-        for (int i = 0; i < vector.size(); i++) {
-            double b1 = vector.getQuick(i);
-            for (int j = 0; j < vector.size(); j++) {
-                double b2 = vector.getQuick(j);
-                matA.add(i, j, scale * b1 * b2);
-            }
-        }
-    }
-
-    static private void appendToB(double scale, TDoubleArrayList vector, TDoubleArrayList[] matB) {
-        for (int row = 0; row < vector.size(); row++) {
-            matB[row].add(scale * vector.getQuick(row));
-        }
-    }
-
-    private TDoubleArrayList[] ordinaryDistances(double[] xy, List<MFNode> nodes) {
-        int diffSize = distsCache.length;
-        double[] tds = null;
-        if (diffSize > 1) {
-            tds = new double[diffSize];
-        }
-        for (MFNode node : nodes) {
-            if (diffSize > 1) {
-                Math2D.distanceAndPartDiffs(xy, node.getCoord(), tds);
-                for (int i = 0; i < distsCache.length; i++) {
-                    distsCache[i].add(tds[i]);
-                }
-            } else {
-                distsCache[0].add(Math2D.distance(xy, node.getCoord()));
-            }
-        }
-        return distsCache;
     }
 
     @Override
     public MFShapeFunction synchronizeClone() {
-        MFShapeFunction result = new MLS(weightFunc.synchronizeClone(), basisFunc.synchronizeClone());
+        MLS result = new MLS();
+        result.weightFunc = weightFunc.synchronizeClone();
+        result.basesFunc = basesFunc.synchronizeClone();
         result.setDiffOrder(getDiffOrder());
+        result.setDimension(getDimension());
         return result;
     }
 
@@ -261,7 +178,71 @@ public class MLS implements MFShapeFunction, SynchronizedClonable<MFShapeFunctio
         return MiscellaneousUtils.simpleToString(this)
                 + '{'
                 + "weightFunc=" + weightFunc
-                + ", basisFunc=" + basisFunc
-                + ", solver=" + solver + '}';
+                + ", basesFunc=" + basesFunc + '}';
+    }
+
+    private void solve(DenseMatrix64F matA, DenseMatrix64F b, DenseMatrix64F x) {
+        if (!CommonOps.solve(matA, b, x)) {
+            throw new IllegalStateException();
+        }
+    }
+
+    private void getDist(MFNode nd, int node_index, double[] dist) {
+        if (dists != null) {
+            for (int i = 0; i < dist.length; i++) {
+                dist[i] = dists[node_index].get(i);
+            }
+            return;
+        }
+        double[] coord = nd.getCoord();
+        double d = 0;
+        for (int i = 0; i < getDimension(); i++) {
+            double t = -coord[i] + pos[i];
+
+            d += t * t;
+            if (getDiffOrder() >= 1) {
+                dist[i + 1] = t;
+            }
+        }
+        d = FastMath.sqrt(d);
+        dist[0] = d;
+        if (d == 0 || getDiffOrder() < 1) {
+            return;
+        }
+        for (int i = 1; i < dist.length; i++) {
+            dist[i] /= d;
+        }
+    }
+
+    private void initMatAB() {
+        int numDiffs = WithDiffOrderUtil.outputLength(getDimension(), getDiffOrder());
+        for (int i = 0; i < numDiffs; i++) {
+            DenseMatrix64F matA = cache.getMatACache(i);
+            DenseMatrix64F matB = cache.getMatBCache(i);
+            CommonOps.fill(matA, 0);
+            CommonOps.fill(matB, 0);
+        }
+    }
+
+    private void pushToMatA(double[] weights, DenseMatrix64F bases) {
+        int numDiffs = WithDiffOrderUtil.outputLength(getDimension(), getDiffOrder());
+        DenseMatrix64F tMat = cache.getMatACache(numDiffs);
+        for (int i = 0; i < numDiffs; i++) {
+            double weight = weights[i];
+            DenseMatrix64F matA = cache.getMatACache(i);
+            CommonOps.multOuter(bases, tMat);
+            CommonOps.add(weight, tMat, matA, matA);
+        }
+    }
+
+    private void pushToMatB(double[] weights, DenseMatrix64F bases, int nodeIndex) {
+        int numDiffs = WithDiffOrderUtil.outputLength(getDimension(), getDiffOrder());
+        for (int i = 0; i < numDiffs; i++) {
+            DenseMatrix64F matB = cache.getMatBCache(i);
+            double weight = weights[i];
+            for (int j = 0; j < basesFunc.basesSize(); j++) {
+                matB.set(j, nodeIndex, weight * bases.get(j));
+            }
+        }
     }
 }
---
+++ src/main/java/net/epsilony/mf/shape_func/MLSCache.java
@@ -0,0 +1,121 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.shape_func;
+
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
+import net.epsilony.tb.analysis.WithDiffOrderUtil;
+import org.ejml.data.DenseMatrix64F;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+class MLSCache {
+
+    static int INDICIAL_CACHE_SIZE = 6;
+    int basesSize = -1;
+    int nodesSize = -1;
+    int diffOrder = -1;
+    int diffSize = -1;
+    int dim = -1;
+    TIntObjectMap<DenseMatrix64F[]> matBCachesMap = new TIntObjectHashMap<>();
+    DenseMatrix64F[] matBCaches = null;
+    DenseMatrix64F[] matACaches = null;
+    DenseMatrix64F[] gammaCaches = null;
+    double[][][] basesCaches = null;
+    DenseMatrix64F[][] basesCacheWrappers = null;
+    private DenseMatrix64F dimSizeCache;
+
+    void setup(int diffOrder, int dim, int basesSize, int nodesSize) {
+        if (diffOrder < 0 || diffOrder > 1) {
+            throw new IllegalArgumentException("only supports diffOrder 0-1, not " + diffOrder);
+        }
+        if (dim < 1 || dim > 3) {
+            throw new IllegalArgumentException("only supports dim 1-3, not " + dim);
+        }
+        if (basesSize < 1) {
+            throw new IllegalArgumentException("basesSize must be positive, not " + basesSize);
+        }
+        if (nodesSize < 1) {
+            throw new IllegalArgumentException("nodesSize must be positive, not " + nodesSize);
+        }
+        if (this.diffOrder != diffOrder || this.dim != dim) {
+            this.diffOrder = diffOrder;
+            this.diffSize = WithDiffOrderUtil.outputLength(dim, diffOrder);
+            if (this.dim != dim) {
+                this.dim = dim;
+                this.dimSizeCache = new DenseMatrix64F(dim);
+            }
+        }
+        if (this.basesSize != basesSize) {
+            this.basesSize = basesSize;
+            newACaches();
+            newGammaCaches();
+            newBasesCaches();
+            matBCachesMap.clear();
+            matBCaches = null;
+            this.nodesSize = -1;
+        }
+        if (this.nodesSize != nodesSize) {
+            this.nodesSize = nodesSize;
+            matBCaches = matBCachesMap.get(nodesSize);
+            if (null == matBCaches) {
+                newBCaches();
+                matBCachesMap.put(nodesSize, matBCaches);
+            }
+        }
+    }
+
+    private void newACaches() {
+        matACaches = new DenseMatrix64F[INDICIAL_CACHE_SIZE];
+        for (int i = 0; i < matACaches.length; i++) {
+            matACaches[i] = new DenseMatrix64F(basesSize, basesSize);
+        }
+    }
+
+    private void newBCaches() {
+        matBCaches = new DenseMatrix64F[INDICIAL_CACHE_SIZE];
+        for (int i = 0; i < matBCaches.length; i++) {
+            matBCaches[i] = new DenseMatrix64F(basesSize, nodesSize);
+        }
+    }
+
+    private void newGammaCaches() {
+        gammaCaches = new DenseMatrix64F[INDICIAL_CACHE_SIZE];
+        for (int i = 0; i < gammaCaches.length; i++) {
+            gammaCaches[i] = new DenseMatrix64F(basesSize,1);
+        }
+    }
+
+    private void newBasesCaches() {
+        basesCaches = new double[INDICIAL_CACHE_SIZE][][];
+        basesCacheWrappers = new DenseMatrix64F[INDICIAL_CACHE_SIZE][];
+        for (int i = 0; i < basesCaches.length; i++) {
+            basesCaches[i] = new double[i + 1][basesSize];
+            basesCacheWrappers[i] = new DenseMatrix64F[i + 1];
+            for (int j = 0; j < basesCaches[i].length; j++) {
+                basesCacheWrappers[i][j] = DenseMatrix64F.wrap(basesSize, 1, basesCaches[i][j]);
+            }
+        }
+    }
+
+    public DenseMatrix64F getGammaCache(int diffIndex) {
+        return gammaCaches[diffIndex];
+    }
+
+    public DenseMatrix64F getMatACache(int diffIndex) {
+        return matACaches[diffIndex];
+    }
+
+    public DenseMatrix64F getMatBCache(int diffIndex) {
+        return matBCaches[diffIndex];
+    }
+
+    public double[][] getBasesCache() {
+        return basesCaches[diffSize];
+    }
+
+    public DenseMatrix64F[] getBasesCacheWraper() {
+        return basesCacheWrappers[diffSize];
+    }
+}
