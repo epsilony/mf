fbb3f9435a443ac8ad77ee4faf93feef582c84b6
Man YUAN <epsilonyuan@gmail.com>
2013-08-20 14:50:16 +0800

    add Py4jServer to help debugging with python

---
+++ src/main/java/net/epsilony/mf/util/Py4jServer.java
@@ -0,0 +1,16 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.util;
+
+import net.epsilony.tb.py4j.Example;
+import py4j.GatewayServer;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class Py4jServer {
+        public static void main(String[] args) {
+        GatewayServer server = new GatewayServer(new Example());
+        server.start();
+    }
+}
