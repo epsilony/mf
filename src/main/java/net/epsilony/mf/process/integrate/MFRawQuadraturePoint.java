34f19c98afa8788f070f4c05db76e581be37cec5
Man YUAN <epsilonyuan@gmail.com>
2013-08-30 16:10:43 +0800

    extract MFRawIntegratePoint

--- src/main/java/net/epsilony/mf/process/integrate/MFRawQuadraturePoint.java
+++ src/main/java/net/epsilony/mf/process/integrate/MFRawQuadraturePoint.java
@@ -1,11 +1,13 @@
 /* (c) Copyright by Man YUAN */
 package net.epsilony.mf.process.integrate;
 
-import net.epsilony.mf.project.MFLoad;
-
 /**
  *
  * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
  */
-public interface MFIntegratePoint extends MFLoad, MFRawQuadraturePoint {
+public interface MFRawQuadraturePoint {
+
+    double getWeight();
+
+    double[] getCoord();
 }
