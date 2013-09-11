be8082e32b5fbef0d0d93c43a0f457bf61d42f51
Man YUAN <epsilonyuan@gmail.com>
2013-09-12 01:27:32 +0800

    add RawMFIntegrateTask and a temp demo to hibernate

--- src/main/java/net/epsilony/mf/process/integrate/RawMFIntegrateTask.java
+++ src/main/java/net/epsilony/mf/process/integrate/RawMFIntegrateTask.java
@@ -2,6 +2,11 @@
 package net.epsilony.mf.process.integrate;
 
 import java.util.List;
+import net.epsilony.mf.project.SimpMFMechanicalProject;
+import net.epsilony.mf.util.persistence.MFHibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 /**
  *
@@ -84,5 +89,23 @@ public class RawMFIntegrateTask<V, N, D> implements MFIntegrateTask<V, N, D> {
     @Override
     public String toString() {
         return "RawMFIntegrateTask{" + "volumeTasks(" + volumeTasks.size() + "), neumannTasks(" + neumannTasks.size() + "), dirichletTasks(" + dirichletTasks.size() + ")}";
+    }
+
+    public static void main(String[] args) {
+        RawMFIntegrateTask mfIntegrateTask = new RawMFIntegrateTask(SimpMFMechanicalProject.genTimoshenkoProjectFactory().produce().getMFIntegrateTask());
+        Configuration config = new Configuration();
+        config.configure();
+        config.setProperty("hibernate.connection.url", "jdbc:sqlite:raw_integrate_task.sqlite");
+        config.setProperty("hibernate.hbm2ddl.auto", "create");
+        SessionFactory factory = MFHibernateUtil.newSessionFactory(config);
+        long start = System.nanoTime();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(mfIntegrateTask);
+        session.getTransaction().commit();
+        session.close();
+        long end = System.nanoTime();
+        System.out.println("end-start = " + (end - start));
+
     }
 }
