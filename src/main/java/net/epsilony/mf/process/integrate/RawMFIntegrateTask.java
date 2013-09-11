d1897a90076ff5be5900f235f8742af5366ff34c
Man YUAN <epsilonyuan@gmail.com>
2013-09-12 01:26:42 +0800

    add a fake property to RawMFIntegratTask to hack 0 var preparedStatment

--- src/main/java/net/epsilony/mf/process/integrate/RawMFIntegrateTask.java
+++ src/main/java/net/epsilony/mf/process/integrate/RawMFIntegrateTask.java
@@ -9,6 +9,24 @@ import java.util.List;
  */
 public class RawMFIntegrateTask<V, N, D> implements MFIntegrateTask<V, N, D> {
 
+    String taskName;
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public RawMFIntegrateTask() {
+    }
+
+    public RawMFIntegrateTask(MFIntegrateTask<V, N, D> task) {
+        volumeTasks = task.volumeTasks();
+        neumannTasks = task.neumannTasks();
+        dirichletTasks = task.dirichletTasks();
+    }
     int id;
 
     @Override
@@ -34,6 +52,18 @@ public class RawMFIntegrateTask<V, N, D> implements MFIntegrateTask<V, N, D> {
 
     public void setDirichletTasks(List<D> dirichletTasks) {
         this.dirichletTasks = dirichletTasks;
+    }
+
+    public List<V> getVolumeTasks() {
+        return volumeTasks;
+    }
+
+    public List<N> getNeumannTasks() {
+        return neumannTasks;
+    }
+
+    public List<D> getDirichletTasks() {
+        return dirichletTasks;
     }
 
     @Override
