41609b5ccd89d3a405b18d6308056e54897a763c
Man YUAN <epsilonyuan@gmail.com>
2013-09-22 12:50:10 +0800

    add 1d integrate task

---
+++ src/main/java/net/epsilony/mf/process/integrate/OneDIntegrateTask.java
@@ -0,0 +1,113 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.process.integrate;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.epsilony.mf.process.integrate.point.MFIntegratePoint;
+import net.epsilony.mf.process.integrate.point.SimpMFIntegratePoint;
+import net.epsilony.tb.analysis.GenericFunction;
+import net.epsilony.tb.quadrature.GaussLegendre;
+import org.apache.commons.math3.analysis.UnivariateFunction;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class OneDIntegrateTask implements MFIntegrateTask<MFIntegratePoint, MFIntegratePoint, MFIntegratePoint> {
+
+    int id;
+    double start;
+    double end;
+    double integrateDomainUpperBound = -1;
+    List<MFIntegratePoint> neumannTasks;
+    List<MFIntegratePoint> dirichletTasks;
+    int degree;
+    double[] quadratureParameters;
+    double[] quadratureWeights;
+    UnivariateFunction volumeFunction;
+
+    public void setVolumeFunction(UnivariateFunction volumeFunction) {
+        this.volumeFunction = volumeFunction;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+        double[][] pointsWeightsByDegree = GaussLegendre.pointsWeightsByDegree(degree);
+        quadratureParameters = pointsWeightsByDegree[0];
+        quadratureWeights = pointsWeightsByDegree[1];
+
+    }
+
+    public void setStart(double start) {
+        this.start = start;
+    }
+
+    public void setEnd(double end) {
+        this.end = end;
+    }
+
+    public void setIntegrateDomainUpperBound(double integrateDomainUpperBound) {
+        if (integrateDomainUpperBound <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.integrateDomainUpperBound = integrateDomainUpperBound;
+    }
+
+    public void setNeumannTasks(List<MFIntegratePoint> neumannTasks) {
+        this.neumannTasks = neumannTasks;
+    }
+
+    public void setDirichletTasks(List<MFIntegratePoint> dirichletTasks) {
+        this.dirichletTasks = dirichletTasks;
+    }
+
+    @Override
+    public List<MFIntegratePoint> volumeTasks() {
+        int domainNum = integrateDomainNum();
+        double domainLength = (end - start) / domainNum;
+        ArrayList<MFIntegratePoint> result = new ArrayList<>(domainNum * quadratureWeights.length);
+        for (int i = 0; i < domainNum; i++) {
+            double domainStart = start + i * domainLength;
+            for (int j = 0; j < quadratureWeights.length; j++) {
+                double weight = quadratureWeights[j] * 0.5 * domainLength;
+                double t = quadratureParameters[j];
+                double pos = domainStart + domainLength * (t + 1) / 2;
+                double load = volumeFunction.value(pos);
+                SimpMFIntegratePoint integratePoint = new SimpMFIntegratePoint();
+                integratePoint.setCoord(new double[]{pos});
+                integratePoint.setDimension(1);
+                integratePoint.setWeight(weight);
+                integratePoint.setLoad(new double[]{load});
+                result.add(integratePoint);
+            }
+        }
+        return result;
+    }
+
+    private int integrateDomainNum() {
+        if (end <= start) {
+            throw new IllegalStateException();
+        }
+        return (int) Math.ceil((end - start) / integrateDomainUpperBound);
+    }
+
+    @Override
+    public List<MFIntegratePoint> neumannTasks() {
+        return neumannTasks;
+    }
+
+    @Override
+    public List<MFIntegratePoint> dirichletTasks() {
+        return dirichletTasks;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+}
