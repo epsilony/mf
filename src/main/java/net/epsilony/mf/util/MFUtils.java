9fc7629d7208992ec8a11a95bc8719e4663284d2
Man YUAN <epsilonyuan@gmail.com>
2013-09-22 14:21:04 +0800

    add MFUtils.rudeAddTo()

---
+++ src/main/java/net/epsilony/mf/util/MFUtils.java
@@ -0,0 +1,17 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.util;
+
+import java.util.List;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class MFUtils {
+
+    public static void RudeAddTo(List src, List dst) {
+        for (Object obj : src) {
+            dst.add(obj);
+        }
+    }
+}
