ad679c0c0a663b58b83ad931c7c7827a77799d90
Man YUAN <epsilonyuan@gmail.com>
2013-09-24 12:39:43 +0800

    add checkPolygon()

--- src/main/java/net/epsilony/mf/geomodel/GeomModel2D.java
+++ src/main/java/net/epsilony/mf/geomodel/GeomModel2D.java
@@ -3,6 +3,8 @@ package net.epsilony.mf.geomodel;
 
 import net.epsilony.tb.solid.Polygon2D;
 import java.util.List;
+import net.epsilony.tb.solid.Line;
+import net.epsilony.tb.solid.Segment;
 
 /**
  *
@@ -13,6 +15,15 @@ public class GeomModel2D implements GeomModel {
     public final static int DIMENSION = 2;
     List<MFNode> spaceNodes;   //allNode except polygon.getVertes()
     private Polygon2D polygon;
+
+    public static boolean checkPolygon(Polygon2D polygon) {
+        for (Segment seg : polygon) {
+            if (!(seg instanceof Line) || !(seg.getStart() instanceof MFNode)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public Polygon2D getPolygon() {
         return polygon;
@@ -28,6 +39,9 @@ public class GeomModel2D implements GeomModel {
     }
 
     public void setPolygon(Polygon2D polygon) {
+        if (!checkPolygon(polygon)) {
+            throw new IllegalArgumentException();
+        }
         this.polygon = polygon;
     }
 
