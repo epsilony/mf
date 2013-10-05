0b6c07365806ff8357fb860d2c152a503c326f7c
Man YUAN <epsilonyuan@gmail.com>
2013-10-05 23:06:56 +0800

    add SegmentSubdomain and corresponding integrate point factory

---
+++ src/main/java/net/epsilony/mf/model/SegmentSubdomain.java
@@ -0,0 +1,39 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.mf.model;
+
+import net.epsilony.tb.solid.Segment;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class SegmentSubdomain implements MFSubdomain {
+
+    Segment segment;
+    double startParameter = 0;
+    double endParameter = 1;
+
+    public Segment getSegment() {
+        return segment;
+    }
+
+    public void setSegment(Segment segment) {
+        this.segment = segment;
+    }
+
+    public double getStartParameter() {
+        return startParameter;
+    }
+
+    public void setStartParameter(double startParameter) {
+        this.startParameter = startParameter;
+    }
+
+    public double getEndParameter() {
+        return endParameter;
+    }
+
+    public void setEndParameter(double endParameter) {
+        this.endParameter = endParameter;
+    }
+}
